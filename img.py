94adbd856afd24c15175d099a2c3fa163d9a0c70
Gustavo Del Santos <gustavodelsantos2015@gmail.com>
2021-10-10 14:17:05 -0700

    teste novo

--- img.py
+++ img.py
@@ -3,6 +3,8 @@ import cv2
 
 # c = r'assets/loli-cha.jpg'
 
+print('Hello')
+
 img = cv2.imread('assets/loli-chan.jpg', -1)
 img = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
 img = cv2.rotate(img, cv2.cv2.ROTATE_90_CLOCKWISE)
